f11ddf0e51068127fc9c00821091537d30b1a1e9
rajeev-baniya <rajeevbaniya11@gmail.com>
2024-11-21 18:45:35 +0545

    feat: fixed banner

--- src/components/common/banner.tsx
+++ src/components/common/banner.tsx
@@ -8,12 +8,12 @@ interface BannerProps {
 const Banner: React.FC<BannerProps> = ({ title, paragraph, image = '/dp.jpg' }) => {
     return (
         <section className="h-[55vh] relative !bg-cover !bg-center" style={{ backgroundImage: `linear-gradient(to right top, rgba(18, 17, 14, 0.6), rgba(18, 17, 14, 0.3)), url(${image})` }}>
-            <div className="absolute bottom-10 left-[5.5%] w-full">
+            <div className="absolute bottom-10 left-[5.5%]">
                 <h1 className="text-[4.6rem] font-extrabold capitalize">
                     {title}
                 </h1>
                 <hr className="hr-short !mx-0 !my-[1.8rem]" />
-                <p className="w-[70%]">{paragraph}</p>
+                <p className="w-[80%]">{paragraph}</p>
 
             </div>
 
